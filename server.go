81a8c0b7199301596d73a94e66293cff29044a30
Олександр <sa44ko@gmail.com>
2023-02-21 23:58:10 +0200

    Added even more perfect commentaries.

--- server.go
+++ server.go
@@ -20,11 +20,13 @@ func timeHandler(responseWriter http.ResponseWriter, request *http.Request) {
 		http.Error(responseWriter, "Method is not supported", http.StatusNotFound)
 		return
 	}
-
+	// Retrieves current time in RFC3339 format, assigning it to the variable "currentTime".
 	currentTime := time.Now().Format(time.RFC3339)
+	// Creates a map variable response with a key "time" and the value currentTime.
 	response := map[string]string{"time": currentTime}
-
+	// Sets the response header "Content-Type" to "application/json".
 	responseWriter.Header().Set("Content-Type", "application/json")
+	// Encodes the response variable as JSON and writes it to the response writer, returning any errors encountered.
 	err := json.NewEncoder(responseWriter).Encode(response)
 	if err != nil {
 		return
