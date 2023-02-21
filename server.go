a9d664d9cf0845026fae7fada2302f750e7d7412
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-02-21 23:21:40 +0200

    Add logic for timeHandler

--- server.go
+++ server.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func timeHandler(w http.ResponseWriter, r *http.Request) {
-	//TODO
+	if r.URL.Path != "/time" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+		}
+	if r.Method != http.MethodGet {
+	http.Error(w, "Method is not supported", http.StatusNotFound)
+	return
+	}
+
+	currentTime := time.Now().Format(time.RFC3339)
+	response := map[string]string{"time": currentTime}
+
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+	return
+	}
 }
 
 func main() {
